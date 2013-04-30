4748468068fed94cb9d60c7bf00b73f97e6fe534
Islam Hasan <hasan@eliteworm.(none)>
2013-04-30 14:59:19 +0300

    preference settings with new changes resolved

--- AndroTaskSchedular/src/com/mesba/taskschedular/SettingsPrefs.java
+++ AndroTaskSchedular/src/com/mesba/taskschedular/SettingsPrefs.java
@@ -1,66 +1,38 @@
 package com.mesba.taskschedular;
 
-import android.annotation.TargetApi;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.mesba.dynamicui.R;
 
-public class SettingsPrefs extends PreferenceActivity implements OnSharedPreferenceChangeListener
-{
+public class SettingsPrefs extends PreferenceActivity implements OnSharedPreferenceChangeListener{
 	
-   private String TAG= "prefs";
-   private DatabaseAdapter dbAdapter;
+	 private String TAG= "prefs";
+	 private DatabaseAdapter dbAdapter;
 
-
-@Override
-    protected void onCreate(final Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        try {
-            getClass().getMethod("getFragmentManager");
-            ForwardByFragment();
-        } catch (NoSuchMethodException e) { 
-            ForwardWithDeps();
-        }
-      
-        PreferenceManager.setDefaultValues(SettingsPrefs.this, R.xml.settings_prefs, false);
-        dbAdapter= new DatabaseAdapter(getApplicationContext());
-   }
-
-    @SuppressWarnings("deprecation")
-    protected void ForwardWithDeps()
-    {
-        addPreferencesFromResource(R.xml.settings_prefs);
-    }
-
-    @TargetApi(11)
-    protected void ForwardByFragment(){
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new PreferenceFrgmentHelper()).commit();
-    }
-    @TargetApi(11)
-    public static class PreferenceFrgmentHelper extends PreferenceFragment{       
-        @Override
-        public void onCreate(final Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.settings_prefs); //outer class private members seem to be visible for inner class, and making it static made things so much easier
-        }
-    }
-    
-    @SuppressWarnings("deprecation")
-	@Override 
-    protected void onResume(){
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.settings_prefs);
+		
+		 PreferenceManager.setDefaultValues(SettingsPrefs.this, R.xml.settings_prefs, false);
+	     dbAdapter= new DatabaseAdapter(getApplicationContext());
+	}
+	
+	@SuppressWarnings("deprecation")
+	protected void onResume(){
         super.onResume();
         // Set up a listener whenever a key changes             
         getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
     }
-    
 	
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPref, String key) {
@@ -87,6 +59,6 @@ public class SettingsPrefs extends PreferenceActivity implements OnSharedPrefere
 		dbAdapter.Close();
 	}
 	
-    
+  
+	
 }
-
